f2698e7146db3bd1c30f80ac0d89488191a1197f
Calcifer <lordofthemaracas@gmail.com>
2017-06-16 17:09:09 -0400

    first commit, successfuly building archive

---
+++ backup.go
@@ -0,0 +1,490 @@
+package main
+
+import (
+	"archive/tar"
+	"bufio"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"fmt"
+	"io"
+	"os"
+	"os/user"
+	"path"
+	"path/filepath"
+	"strings"
+
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+const (
+	usage = `Usage:
+	backup [--help] <build|restore> [--help] [OPTIONS]`
+
+	help = usage + `
+
+Used to build and restore a backup of a user directory according to a list file
+specifying files to be included and excluded.
+
+To see more about each command, use 'backup <command> --help'.
+
+Commands:
+	backup     builds a backup
+	restore    restores from a backup file`
+)
+
+type exitError struct {
+	msg  string
+	code int
+}
+
+func (e exitError) Error() string {
+	return e.msg
+}
+
+type homeError struct {
+	who    string
+	reason string
+}
+
+func (e homeError) Error() string {
+	if e.who == "" {
+		return "Unable to find current user's home directory: " + e.reason
+	}
+	return fmt.Sprintf("Unable to find home directory of user %s: %s", e.who, e.reason)
+}
+
+func main() {
+	var exitCode int = program()
+	if exitCode != 0 {
+		os.Exit(exitCode)
+	}
+}
+
+func program() int {
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, usage)
+		return 1
+	}
+
+	var err error
+	switch os.Args[1] {
+	case "build":
+		err = build(os.Args[2:])
+	case "restore":
+		err = restore(os.Args[2:])
+	case "--help", "-h":
+		fmt.Println(help)
+		return 0
+	default:
+		fmt.Fprintf(os.Stderr, "Unrecognized command '%s'\n", os.Args[1])
+		return 1
+	}
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		if exit, ok := err.(exitError); ok {
+			return exit.code
+		}
+		return 2
+	}
+	return 0
+}
+
+func build(args []string) error {
+	listPaths := []string{}
+	outPaths := []string{}
+	for i := 0; i < len(args); i += 2 {
+		switch args[i] {
+		case "--help", "-h":
+			fmt.Println(`Usage:
+	backup build [--help] [-l LIST] [-o OUTPUT]
+
+The build command backs up a list of files as determined by the provided lists
+and saves them in an AES encrypted tarball.The list files operate in stages,
+indicated by either [include] or [exclude] markers.  After each marker, backup
+looks for a newline-delimited list of glob patterns to match agains files.  You
+can add an unlimited number of stages of [include] and [exclude] that will be
+evaluated in order.  If the first marker is an include, it is assumed everything
+else is excluded by default, and if the first stage is an exclude, it is assumed
+everything in your user directory is included by default.
+
+You can set multiple list files and output paths by using their options twice,
+as in 'backup -l list1 -l list2 -o backup1 -o backup2'.  In this case, the list
+files will be loaded in the order that they're listed.
+
+Options:
+	-h, --help      this help message
+	-l, --list      file that contains what's to be excluded and included in the backup, defaults to ./backup.list
+	-o, --output    where to store the backup file, by default the output is printed to standard out`)
+			return nil
+
+		case "-l", "--list":
+			s := tryGetArg(args, i+1)
+			if s == "" {
+				return exitError{
+					msg:  fmt.Sprintf("Expected argument after '%s'", args[i]),
+					code: 1,
+				}
+			}
+			listPaths = append(listPaths, s)
+		case "-o", "--output":
+			s := tryGetArg(args, i+1)
+			if s == "" {
+				return exitError{
+					msg:  fmt.Sprintf("Expected argument after '%s'", args[i]),
+					code: 1,
+				}
+			}
+			outPaths = append(outPaths, s)
+		}
+	}
+
+	if len(listPaths) == 0 {
+		listPaths = append(listPaths, "backup.list") // the default list file
+	}
+	return runBuild(listPaths, outPaths)
+}
+
+func runBuild(listPaths []string, outPaths []string) error {
+	stages := []buildStage{}
+	for _, listPath := range listPaths {
+		file, err := os.Open(listPath)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to open list file '%s': %s\n", listPath, err.Error())
+			continue
+		}
+
+		stages, err = loadStages(file, stages)
+		if err != nil {
+			return err
+		}
+		file.Close()
+	}
+
+	fileList, err := compileStages(stages)
+	if err != nil {
+		return err
+	}
+
+	var output io.Writer
+	if len(outPaths) == 0 {
+		output = os.Stdout
+	} else {
+		var opened []io.Writer
+		for _, outPath := range outPaths {
+			file, err := os.OpenFile(outPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+			if err != nil {
+				return err
+			}
+			defer file.Close()
+			opened = append(opened, file)
+		}
+		if len(opened) == 1 {
+			output = opened[0]
+		} else {
+			output = io.MultiWriter(opened...)
+		}
+	}
+
+	// aesStream, err := setupCryptoStream(output)
+	// if err != nil {
+	// 	return err
+	// }
+	// defer aesStream.Close()
+
+	// archiver := tar.NewWriter(aesStream)
+	// defer archiver.Close()
+
+	archiver := tar.NewWriter(output)
+	defer archiver.Close()
+
+	for _, path := range fileList {
+		err = archiveFile(archiver, path)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// loadStages operates similarly to the append function
+func loadStages(file *os.File, stages []buildStage) ([]buildStage, error) {
+	var stage *buildStage
+	scanner := bufio.NewScanner(file)
+	for i := 1; scanner.Scan(); i++ {
+		line := strings.TrimSpace(scanner.Text())
+		switch line {
+		case "[include]":
+			stages = append(stages, buildStage{
+				include: true,
+				source:  file.Name(),
+			})
+			stage = &stages[len(stages)-1]
+		case "[exclude]":
+			stages = append(stages, buildStage{
+				include: false,
+				source:  file.Name(),
+			})
+			stage = &stages[len(stages)-1]
+		case "": // don't add empty lines
+		default:
+			if stage == nil {
+				continue
+			}
+
+			// normalize the globs
+			if strings.HasPrefix(line, "~"+string(os.PathSeparator)) || line == "~" {
+				// insert the current user's home directory
+				home, err := getHomeDir("")
+				if err != nil || home == "" {
+					return nil, homeError{who: "", reason: fmt.Sprintf("needed for substitution in '%s' on line %d", file.Name(), i)}
+				}
+				line = path.Join(home, line[1:])
+			} else if strings.HasPrefix(line, "~") {
+				// do user-specific home replacement (e.g. ~bob/Documents)
+				var nameEnd int = strings.IndexRune(line, os.PathSeparator)
+				if nameEnd == -1 {
+					nameEnd = len(line) // the entire line is a user directory
+				}
+				name := line[1:nameEnd]
+				home, err := getHomeDir(name)
+				if err != nil {
+					return nil, homeError{who: name, reason: fmt.Sprintf("needed for substitution in '%s' on line %d", file.Name(), i)}
+				}
+				line = path.Join(home, line[nameEnd:])
+			}
+			stage.rules = append(stage.rules, buildRule{glob: line, line: i})
+		}
+	}
+	return stages, nil
+}
+
+// compileStages used the rules set out in stages to build a list of files to
+// back up
+func compileStages(stages []buildStage) ([]string, error) {
+	if len(stages) == 0 {
+		return nil, nil
+	}
+
+	// first, build a list of all the exclusion rules, in order
+	exclusions := []string{}
+	for _, stage := range stages {
+		if !stage.include { // !include = exclude
+			for _, rule := range stage.rules {
+				exclusions = append(exclusions, rule.glob)
+			}
+		}
+	}
+
+	list := []string{}
+	for _, stage := range stages {
+		if stage.include {
+			for _, rule := range stage.rules {
+				var glob []string
+				glob, _ = filepath.Glob(rule.glob)
+				// now check the files we've found against all future exclusions
+				for _, file := range glob {
+					filepath.Walk(file, func(wpath string, info os.FileInfo, err error) error {
+						excluded := false
+						var full, base bool
+						for _, excl := range exclusions {
+							full, _ = filepath.Match(excl, wpath)
+							base, _ = filepath.Match(excl, path.Base(wpath))
+							if full || base {
+								excluded = true
+								break
+							}
+						}
+						if skipFileType(info) {
+							return nil
+						} else if info.IsDir() {
+							if excluded {
+								// don't recurse into excluded directories
+								return filepath.SkipDir
+							}
+						} else if !excluded {
+							list = append(list, wpath)
+						}
+						return nil
+					})
+				}
+			}
+		} else {
+			// we no longer need to check against the rules listed in this stage
+			// because they're listed before any more inclusions we encounter
+			exclusions = exclusions[len(stage.rules):]
+		}
+	}
+
+	return list, nil
+}
+
+// skipFileType checks to see if a file can be skipped based on its type stored
+// in the mode.  Types that aren't skipped are: regular, directory, symlink, and
+// hardlinks.  Temporary files are skipped.  A return value of true indicates
+// the file should be skipped, false indicates it should be kept.
+func skipFileType(info os.FileInfo) bool {
+	if info.Mode()&os.ModeTemporary != 0 {
+		return true
+	}
+	switch info.Mode() & os.ModeType {
+	case os.ModeDir, os.ModeSymlink:
+		return false
+	}
+	if info.Mode().IsRegular() {
+		return false
+	}
+	return true
+}
+
+type ioCombo struct {
+	r io.Reader
+	w io.Writer
+}
+
+func (io ioCombo) Read(data []byte) (int, error) {
+	return io.r.Read(data)
+}
+
+func (io ioCombo) Write(data []byte) (int, error) {
+	return io.w.Write(data)
+}
+
+type managedWriter struct {
+	w        io.Writer
+	password []byte
+}
+
+func (w managedWriter) Write(data []byte) (int, error) {
+	return w.w.Write(data)
+}
+
+func (w managedWriter) Close() error {
+	for i := range w.password {
+		w.password[i] = 0
+	}
+	return nil
+}
+
+func setupCryptoStream(output io.Writer) (io.WriteCloser, error) {
+	var term *terminal.Terminal = terminal.NewTerminal(ioCombo{r: os.Stdin, w: os.Stdout}, "Password: ")
+	password, err := term.ReadPassword("Password: ")
+	if err != nil {
+		return nil, err
+	}
+
+	// add padding/stip end to make password 32 bytes long to enable AES-256
+	if len(password) < 32 {
+		password += string(make([]byte, 32-len(password)))
+	} else if len(password) > 32 {
+		password = password[:32]
+	}
+
+	var block cipher.Block
+	block, _ = aes.NewCipher([]byte(password))
+
+	var iv [aes.BlockSize]byte
+	_, err = rand.Read(iv[:])
+	if err != nil {
+		return nil, err
+	}
+
+	// first, save the IV in the first aes.BlockSize (16) bytes of the output
+	_, err = output.Write(iv[:])
+	if err != nil {
+		return nil, err
+	}
+	stream := cipher.NewOFB(block, iv[:])
+	return managedWriter{
+		password: []byte(password),
+		w:        &cipher.StreamWriter{S: stream, W: output},
+	}, nil
+}
+
+func archiveFile(archiver *tar.Writer, path string) error {
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to open '%s': %s\n", path, err.Error())
+		return nil
+	}
+	defer file.Close()
+
+	header := buildTarHeader(path)
+	if header == nil {
+		return nil
+	}
+	err = archiver.WriteHeader(header)
+	if err != nil {
+		return nil
+	}
+	if header.Typeflag == tar.TypeSymlink {
+		// don't write anything for symlinks, the target is contained in the header
+		return nil
+	}
+	_, err = io.Copy(archiver, file)
+	if err != nil {
+		return fmt.Errorf("Error archiving '%s': %s", path, err.Error())
+	}
+	return nil
+}
+
+func getHomeDir(who string) (string, error) {
+	var info *user.User
+	var err error
+	if who == "" {
+		info, err = user.Current()
+	} else {
+		info, err = user.Lookup(who)
+	}
+	if err != nil {
+		return "", err
+	}
+	return info.HomeDir, nil
+}
+
+// stage represents a single [include/exclude] directive
+type buildStage struct {
+	// if false, exclude
+	include bool
+	// source is the name of the file from which this stage originates
+	source string
+	rules  []buildRule
+}
+
+type buildRule struct {
+	glob string
+	line int
+}
+
+func restore(args []string) error {
+	var backupPath string
+	for _, arg := range args {
+		switch arg {
+		case "--help", "-h":
+			fmt.Println(`Usage:
+	backup restore [--help] <backup_file>
+
+Restores the files provided in the given backup archive.`)
+			return nil
+
+		default:
+			if backupPath != "" {
+				return exitError{
+					msg:  "Can only restore from one backup at a time",
+					code: 1,
+				}
+			}
+			backupPath = arg
+		}
+	}
+
+	return nil
+}
+
+func tryGetArg(args []string, index int) string {
+	if index < 0 || index > len(args) {
+		return ""
+	}
+	return args[index]
+}
---
+++ tar_linux.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"archive/tar"
+	"os"
+	"time"
+	"unsafe"
+
+	"golang.org/x/sys/unix"
+)
+
+/*
+#cgo CFLAGS: -std=c99
+#define _POSIX_SOURCE
+#include <pwd.h>
+#include <grp.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+typedef struct names_t {
+	char* username;
+	char* groupname;
+} names_t;
+
+names_t convertIds(uint32_t uid, uint32_t gid) {
+	names_t names;
+	names.username = NULL;
+	names.groupname = NULL;
+
+	struct passwd pwd;
+	struct passwd *user_result;
+	size_t buf_len = 100;
+	char* ubuf = malloc(buf_len);
+	getpwuid_r((uid_t)uid, &pwd, ubuf, buf_len, &user_result);
+
+	if (user_result == NULL) {
+		free(ubuf);
+	} else {
+		names.username = pwd.pw_name;
+	}
+
+	struct group grp;
+	struct group *grp_result;
+	char* gbuf = malloc(buf_len);
+	getgrgid_r((gid_t)gid, &grp, gbuf, buf_len, &grp_result);
+
+	if (grp_result == NULL) {
+		free(gbuf);
+	} else {
+		names.groupname = grp.gr_name;
+	}
+
+	return names;
+}
+*/
+import "C"
+
+// buildTarHeader runs Lstat on the provided path and returns a tar header with
+// all the information converted over.  Returns nil on error.
+func buildTarHeader(path string) *tar.Header {
+	var info unix.Stat_t
+	err := unix.Lstat(path, &info)
+	if err != nil {
+		return nil
+	}
+
+	var names C.names_t = C.convertIds(C.uint32_t(info.Uid), C.uint32_t(info.Gid))
+	defer C.free(unsafe.Pointer(names.username))
+	defer C.free(unsafe.Pointer(names.groupname))
+
+	username := C.GoString(names.username)
+	groupname := C.GoString(names.groupname)
+
+	linkname, _ := os.Readlink(path)
+
+	var tarType byte
+	switch info.Mode & unix.S_IFMT {
+	case unix.S_IFDIR:
+		tarType = tar.TypeDir
+	case unix.S_IFLNK:
+		tarType = tar.TypeSymlink
+	case unix.S_IFBLK:
+		tarType = tar.TypeBlock
+	case unix.S_IFCHR:
+		tarType = tar.TypeChar
+	case unix.S_IFIFO:
+		tarType = tar.TypeFifo
+	default:
+		tarType = tar.TypeReg
+	}
+
+	return &tar.Header{
+		Name:       path,
+		Mode:       int64(info.Mode),
+		Uid:        int(info.Uid),
+		Gid:        int(info.Gid),
+		Size:       info.Size,
+		Uname:      username,
+		Gname:      groupname,
+		ModTime:    time.Unix(info.Mtim.Unix()),
+		Typeflag:   tarType,
+		Linkname:   linkname,
+		AccessTime: time.Unix(info.Atim.Unix()),
+		ChangeTime: time.Unix(info.Ctim.Unix()),
+	}
+}
